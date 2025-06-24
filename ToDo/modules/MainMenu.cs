44a4f46647841768adae94408ed528527c979084
unknown <admin@admin>
2025-06-24 21:59:33 +0300

    Add main menu form close #1

---
+++ ToDo/modules/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo.modules
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+            Settings();
+        }
+        private void Settings()
+        {
+            // Styles
+            this.Text = "ToDo | Главное меню";
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
